561cd854b8c647d605155c79013fc08e3d53a6f9
mmalghamdi <masheal.alghamdi@kaust.edu.sa>
2021-12-23 12:28:33 +0300

    Test git

--- tutorials/01-basics/feedforward_neural_network/main.py
+++ tutorials/01-basics/feedforward_neural_network/main.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 
-
+#Hi this is me trying to test git push :)
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
